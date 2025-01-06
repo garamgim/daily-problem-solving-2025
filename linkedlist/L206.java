f769396675c20bff69fc38ed1d5a80037e7b2b25
garamgim <garamgim@gmail.com>
2025-01-06 15:35:22 +0900

    250106

--- linkedlist/L206.java
+++ linkedlist/L206.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class L206 {
+/*
+ * LeetCode Easy 206 Reverse Linked List
+ * https://leetcode.com/problems/reverse-linked-list/
+ * */
+
+public class L206 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    class Solution {
+        public ListNode reverse(ListNode prev, ListNode curr) {
+            if (curr == null) return prev;
+
+            ListNode next = curr.next;
+            curr.next = prev;
+            return reverse(curr, next);
+        }
+
+        public ListNode reverseList(ListNode head) {
+            return reverse(null, head);
+        }
+    }
 }
