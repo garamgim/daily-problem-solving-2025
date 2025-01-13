7b3421a69e5146da25a97efbed3d6d54d93be8e8
garamgim <garamgim@gmail.com>
2025-01-13 23:32:55 +0900

    250113

--- stack/L739.java
+++ stack/L739.java
@@ -9,20 +9,21 @@ import java.util.Deque;
 public class L739 {
     class Solution {
         public int[] dailyTemperatures(int[] temperatures) {
-            Deque<int[]> stk = new ArrayDeque<>();
+            Deque<Integer> stk = new ArrayDeque<>();
             int[] ans = new int[temperatures.length];
 
+            // 온도 배열을 순회
             for (int i = 0; i < temperatures.length; i++) {
-                int curr = temperatures[i];
+                int curr = temperatures[i]; // 현재 날짜의 온도
 
-                // 스택이 비어있지 않고, 현재 온도가 스택의 최상단에 있는 온도보다 높을 경우
-                while (!stk.isEmpty() && stk.peek()[0] < curr) {
-                    int[] prev = stk.pop();     // 스택에서 이전 온도와 인덱스를 꺼냄
-                    ans[prev[1]] = i - prev[1]; // 현재 인덱스와 이전 인덱스 차이를 결과에 저장
+                // 스택이 비어있지 않고, 현재 온도가 스택의 최상단 인덱스의 온도보다 높을 경우
+                while (!stk.isEmpty() && temperatures[stk.peek()] < curr) {
+                    int prev = stk.pop(); // 스택에서 이전 인덱스를 꺼냄
+                    ans[prev] = i - prev; // 현재 인덱스와 이전 인덱스 차이를 결과에 저장
                 }
 
-                // 현재 온도와 인덱스를 스택에 추가
-                stk.push(new int[]{curr, i});
+                // 현재 인덱스를 스택에 추가
+                stk.push(i);
             }
 
             return ans;
